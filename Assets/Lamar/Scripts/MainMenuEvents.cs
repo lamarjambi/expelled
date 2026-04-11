16313f5b517ea8e6da14453c6e5e7f71853f0cf0
Lamar Jambi <lmj8416@nyu.edu>
2026-04-10 23:29:45 -0400

    starting level design

--- Assets/Lamar/Scripts/MainMenuEvents.cs
+++ Assets/Lamar/Scripts/MainMenuEvents.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4ceeab4cd8d2131b04b7e2026d0efbb380c69be81fdfcecfb8b3bb980163184
-size 1253
+oid sha256:3f2b90c2f5fd51e69995636c168ad2a261decca5aac1fe8b914048f78297c816
+size 1251
