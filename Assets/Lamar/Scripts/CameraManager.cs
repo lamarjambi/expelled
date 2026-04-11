7f725c0fe6be25427dce69ea1a0cbaaa8a58fc6d
Lamar Jambi <lmj8416@nyu.edu>
2026-04-11 00:10:25 -0400

    added z dimention

--- Assets/Lamar/Scripts/CameraManager.cs
+++ Assets/Lamar/Scripts/CameraManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b29edc0c72dad775167441a029e429dc9827e6cf2de3c7f1eb2d1ea0faa3580b
-size 1356
+oid sha256:b977ed784f3f3a60e7d1b0b554061134f5a9d0aba015f981fe618d51ea41834b
+size 1770
