605f80a83c4528a6f0c94cc72ae7d4623971812b
Lamar Jambi <lmj8416@nyu.edu>
2026-04-12 03:43:39 -0400

    added attack animation + polytope is URP

--- Assets/Lamar/Scripts/MainMenuEvents.cs
+++ Assets/Lamar/Scripts/MainMenuEvents.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f2b90c2f5fd51e69995636c168ad2a261decca5aac1fe8b914048f78297c816
-size 1251
+oid sha256:e2879ce6d660518a07ca178385d913df390b6e0e86dccbb6fbca6df74be0aa8e
+size 1252
--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01cb5f828ba42cd7e8158e05789686fcfce51e1f7980a5e0e62a77afb284c293
-size 13838
+oid sha256:d94994bb0552f42b08156a81d78df4787a29ffcaefa58460820d2148c7bfacba
+size 14247
