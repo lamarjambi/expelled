42a4c39616512b8418f895abad66afb7da3c3d0a
Gordon Rose <gar9048@nyu.edu>
2026-04-14 18:22:54 -0400

    merged custom controller into new player script

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91c5d4cf1888ef18e131a8b917d3108a43b9c0ed1a5fe9c17a8f6ff4a993c5cd
-size 15894
+oid sha256:2f427982cbb6a4ec3e2516f47e9a68175401491bb2f609fcab9c4e8972d8f492
+size 15998
