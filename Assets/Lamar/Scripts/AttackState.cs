9f2ea081c294118fab48ecf385cc141772258a8f
Gordon Rose <gar9048@nyu.edu>
2026-04-15 17:43:07 -0400

    removed rootmotion toggle from AttackState

--- Assets/Lamar/Scripts/AttackState.cs
+++ Assets/Lamar/Scripts/AttackState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:608be8c15affd1ce2596ba9f7c28dcb2b856dc53e543e4d3dcb70ba95cd7a0cd
-size 1704
+oid sha256:b9846fb74bffb5b9440a7618e2021d6eafb3238d4b241d5107222a1935b7cd49
+size 1708
--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f427982cbb6a4ec3e2516f47e9a68175401491bb2f609fcab9c4e8972d8f492
-size 15998
+oid sha256:a8c1b0ccd280d2de80ab2c13c5830d878fb594b1f0e91c12f9a1b1d8b7d362ca
+size 16275
