c0b6f52f205fa65fc8723fac492c88dc5a61d622
Lamar Jambi <lmj8416@nyu.edu>
2026-04-22 18:41:26 -0400

    added first healing plant

---
+++ Assets/Lamar/Scripts/HealingArea.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:edac72991e8fe5d65761187a1cd13537282541c078c0d57cbb5402810a1f69bb
+size 681
--- Assets/Lamar/Scripts/HealthSystem.cs
+++ Assets/Lamar/Scripts/HealthSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87b7f054996e026779402028d391811cc0d74a4712801f2d5f3701ddda447a91
-size 1202
+oid sha256:78ec062037bbc4bf55a92034aaca6843fd55232e9c56c9bade54855082a17fed
+size 1339
