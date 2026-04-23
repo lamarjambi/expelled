89e5ab7b93157942551200d363bbc1d3d5799e13
Lamar Jambi <lmj8416@nyu.edu>
2026-04-23 19:21:42 -0400

    minor level design changed to gamescene2

--- Assets/Lamar/Scripts/EnemyManager.cs
+++ Assets/Lamar/Scripts/EnemyManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48c0b3ea5503f0d1d900b09d4b5b2aaead91b347c04a19a78aaaca2528224230
-size 502
+oid sha256:aae3d58ba068d9bc1282bfdfea1f0e59d99657b9e59c35a89f42fd423d6b3e1c
+size 591
