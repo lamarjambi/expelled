b99868b9437979bdece26f5e31527b4bd1338aca
Gordon Rose <gar9048@nyu.edu>
2026-04-23 12:20:36 -0400

    hurtbox emits in front of player

--- Assets/Lamar/Scripts/DamageDealer.cs
+++ Assets/Lamar/Scripts/DamageDealer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:661152da9c1698b9ad48b38bc20300fbdc7b510d36d1b3daad78e74036b5b726
-size 1490
+oid sha256:02f61a22023b28aca6b857d802dab5b371287eb8ac3e12fc161cb2a69009b042
+size 2748
