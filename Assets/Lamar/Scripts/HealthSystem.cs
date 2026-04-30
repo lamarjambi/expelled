06e4f61a2169189471d0f6b30fa1430fad22c3c1
Gordon Rose <gar9048@nyu.edu>
2026-04-30 02:30:16 -0400

    player hit sfx

--- Assets/Lamar/Scripts/HealthSystem.cs
+++ Assets/Lamar/Scripts/HealthSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d675d6078d4ed2b7ef325057102529b826cec22fa436e14e1b4cf04887e4f3d
-size 2117
+oid sha256:5b3ee02802a3e4d9f0f0a9bd992e9e254b736bd1f210975699e9ab7e0b65a3b3
+size 2207
