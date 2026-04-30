3ff8f895da844c020c423652d5da717bd653dc5a
Gordon Rose <gar9048@nyu.edu>
2026-04-30 03:00:04 -0400

    door open sfx

--- Assets/Lamar/Scripts/DoorController.cs
+++ Assets/Lamar/Scripts/DoorController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:041bd9d19ef9cce07e42ccd98529b8eff5f28a64b028150d2d71769aa5a09217
-size 492
+oid sha256:f154a65ca877f5335484edfc3496592c988cfba4c185f890f3c3c3a76205c9d3
+size 564
