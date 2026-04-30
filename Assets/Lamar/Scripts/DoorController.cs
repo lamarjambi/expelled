0702ded5dd7bf83c60435cb003187f2ea72b2975
Gordon Rose <gar9048@nyu.edu>
2026-04-30 03:43:36 -0400

    fixed doors not opening when they should

--- Assets/Lamar/Scripts/DoorController.cs
+++ Assets/Lamar/Scripts/DoorController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f154a65ca877f5335484edfc3496592c988cfba4c185f890f3c3c3a76205c9d3
-size 564
+oid sha256:671f32dc0b2967b41801b877be22ee57bfd7407724a03d31af70fec582c81828
+size 591
