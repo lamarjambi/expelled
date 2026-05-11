19e456a16960d8534410103d327073d74745daaa
Lamar Jambi <lmj8416@nyu.edu>
2026-05-11 18:19:45 -0400

    mastervolume in settings

--- Assets/Lamar/Scripts/Settings.cs
+++ Assets/Lamar/Scripts/Settings.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5404c467a106e63380f04796a20ba8c43d6ef48e6d2c8dce60a13c0df8e9113
-size 318
+oid sha256:17a1d8f5fdcd2d1cd7e429339032f5600b941e5eb811b0e5580bad1253923d64
+size 390
